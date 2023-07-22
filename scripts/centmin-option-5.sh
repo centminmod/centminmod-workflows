39154ba55f7aadff88334b136960c7376f3a43c1
George Liu (eva2000) <eva2000@centminmod.com>
2023-07-23 08:25:15 +1000

    add php-fpm upgrade test support to workflows

---
+++ scripts/centmin-option-5.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/expect -f
+set timeout -1
+spawn /usr/local/src/centminmod/centmin.sh
+expect "Enter option"
+send -- "4\r"
+expect "PHP Upgrade/Downgrade - Would you like to continue?"
+send -- "y\r"
+expect "Enter PHP Version number you want to upgrade/downgrade to"
+send -- "8.0.29\r"
+expect "Do you want to use Zend OPcache"
+send -- "\r"
+expect "Enter option"
+send -- "24\r"
+expect eof
