72f282c454db27cd1012c5cb50789d37878fdf45
George Liu (eva2000) <eva2000@centminmod.com>
2023-08-07 12:46:26 +1000

    add php 7.3 & 7.4 scripts

---
+++ scripts/centmin-option-5-php7.3.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/expect -f
+set timeout -1
+spawn /usr/local/src/centminmod/centmin.sh
+expect "Enter option"
+send -- "5\r"
+expect "PHP Upgrade/Downgrade - Would you like to continue?"
+send -- "y\r"
+expect "Enter PHP Version number you want to upgrade/downgrade to"
+send -- "7.3.33\r"
+expect "Do you still want to continue?"
+send -- "y\r"
+expect "Do you want to use Zend OPcache"
+send -- "\r"
+expect "Enter option"
+send -- "24\r"
+expect eof
