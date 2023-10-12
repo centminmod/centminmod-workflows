ae00aee071607785e4564353e9d139eabd682bd5
George Liu (eva2000) <eva2000@centminmod.com>
2023-10-12 17:05:50 +1000

    add nginx master branch build tests to comparison workflow

---
+++ scripts/centmin-option-4-master.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/expect -f
+set timeout -1
+spawn /usr/local/src/centminmod/centmin.sh
+expect "Enter option"
+send -- "4\r"
+expect "Nginx Upgrade - Would you like to continue?"
+send -- "y\r"
+expect "Install which version of Nginx"
+send -- "master\r"
+expect "Do you still want to continue?"
+send -- "y\r"
+expect "Enter option"
+send -- "24\r"
+after 30000
+expect eof
