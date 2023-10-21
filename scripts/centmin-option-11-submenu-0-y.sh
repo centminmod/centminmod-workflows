7ca58bf1bd2641aedfe86b0ec1fab4ea540db4ab
George Liu (eva2000) <eva2000@centminmod.com>
2023-10-22 01:35:16 +1000

    add MySQL root password reset tests for comparison workflows

---
+++ scripts/centmin-option-11-submenu-0-y.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/expect -f
+set timeout -1
+spawn /usr/local/src/centminmod/centmin.sh
+expect "Enter option"
+send -- "11\r"
+expect "Enter option"
+send -- "0\r"
+expect "Do you want to continue?"
+send -- "y\r"
+expect "Do you know the existing MySQL root password set in /root/.my.cnf?"
+send -- "y\r"
+expect "Enter option"
+send -- "11\r"
+expect "Enter option"
+send -- "24\r"
+after 30000
+expect eof
