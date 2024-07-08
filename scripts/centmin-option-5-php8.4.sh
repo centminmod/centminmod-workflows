636eeeb5da4cb2379d3ff349a2e065e3aa783ded
George Liu (eva2000) <eva2000@centminmod.com>
2024-07-08 12:43:23 +1000

    add PHP 8.4.0 alpha1 test script

---
+++ scripts/centmin-option-5-php8.4.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/expect -f
+set timeout -1
+spawn /usr/local/src/centminmod/centmin.sh
+expect "Enter option"
+send -- "5\r"
+expect "PHP Upgrade/Downgrade - Would you like to continue?"
+send -- "y\r"
+expect "Enter PHP Version number you want to upgrade/downgrade to"
+send -- "8.4.0alpha1\r"
+expect "Do you still want to continue?"
+send -- "y\r"
+expect "Do you want to use Zend OPcache"
+send -- "\r"
+expect "Enter option"
+send -- "24\r"
+after 30000
+expect eof
